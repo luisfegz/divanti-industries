d1d54c0d48b0074b400b8c07e46e9cc9f37d8278
Luis Felipe <luisfelipegaitanzambrano@gmail.com>
2024-06-16 19:20:56 -0500

    36 XD

--- src/app/tech1/page.tsx
+++ src/app/tech1/page.tsx
@@ -7,62 +7,66 @@ import Image from "next/image";
 const Page = () => {
   return (
     <>
-     <MaxWidthWrapper className='mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center'>
+     <MaxWidthWrapper className='mb-0 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center'>
           <BoxReveal boxColor={"#fff679"} duration={0.5}>
             <h1 className='max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl'>
               <span className='text-yellow-400'>U</span>
               AV EN CONSTRUCCIÓN{' '}
             </h1>
           </BoxReveal>
-          <BoxReveal boxColor={"#fff679"} duration={0.5}>
-            <h2 className="mt-3 max-w-prose text-xl text-zinc-700 sm:text-2xl">
-              UAV en construcción
-            </h2>
-          </BoxReveal> 
       </MaxWidthWrapper>
-      
-      <MaxWidthWrapper className='mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center'>
-
-        <ol className='my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0'>
-            <li className='md:flex-1'>
-              <div className='flex flex-col space-y-2  py-2 pl-3 pr-3 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
-                <div className='rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl lg:p-4' style={{ maxWidth: '800px', margin: 'auto', padding: '1rem' }}>
-                  <Image
-                    src='/4.jpeg'
-                    alt='product preview'
-                    width={1024}
-                    height={1024}
-                    quality={100}
-                    className='rounded-md bg-white shadow-2xl ring-1 ring-gray-900/10'
-                  />
+      <MaxWidthWrapper className='mb-2 mt-0 flex flex-col items-center justify-center text-center'>
+            <div className='mx-auto mb-2 max-w-5xl mt-12'>
+                <div className='mb-0 px-6 lg:px-8'>
+                  <div className='mx-auto max-w-2xl sm:text-center'>
+                    <p className='text-lg text-justify text-gray-600'>
+                      • Inspecciones estructurales en alturas o en grandes distancias.
+                    </p>
+                    <br />
+                    <div className='rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl lg:p-4' style={{ maxWidth: '800px', margin: 'auto', padding: '1rem' }}>
+                      <Image
+                        src='/tech1.jpg'
+                        alt='product preview'
+                        width={1024}
+                        height={1024}
+                        quality={100}
+                        className='rounded-md bg-white shadow-2xl ring-1 ring-gray-900/10'
+                      />
+                    </div>
+                    <br />
+                    <p className='text-lg mt-4 text-justify text-gray-600 mb-12'>
+                      Inspecciones estructurales en alturas y grandes distancias mediante drones es otra solucion
+                      que tendras a disposicion con nuestro departamento , el uso de drones representa una innovadora 
+                      solución que aprovecha las tecnologías actuales para mejorar diversos aspectos clave de la 
+                      gestión de infraestructuras.
+                      <br /> <br />
+                      En cuanto a los materiales, los drones se equipan con una variedad de sensores avanzados 
+                      que permiten detectar anomalías en la estructura, como grietas, corrosión o deformaciones, 
+                      proporcionando una evaluación precisa del estado de los materiales. Esto permite una i
+                      dentificación temprana de problemas potenciales, lo que a su vez facilita la planificación 
+                      de mantenimiento preventivo y la prolongación de la vida útil de la infraestructura.
+                      <br /> <br />
+                      La costo-efectividad de esta solución radica en la reducción de los costos asociados con la 
+                      mano de obra y el tiempo requerido para llevar a cabo inspecciones tradicionales. 
+                      <br /> <br />
+                      Al utilizar drones, se pueden inspeccionar grandes áreas en menos tiempo y con menos recursos
+                      humanos, lo que resulta en una considerable reducción de los costos operativos a largo plazo.
+                      En términos de eficiencia, los drones son capaces de cubrir grandes distancias y alturas en 
+                      un tiempo considerablemente menor que los métodos convencionales. Esto permite una mayor 
+                      frecuencia de inspecciones sin comprometer la calidad de los datos recopilados, lo que a su 
+                      vez contribuye a una gestión más efectiva de la infraestructura y una respuesta más rápida 
+                      ante posibles problemas.
+                      <br /> <br />
+                      La calidad de los datos obtenidos mediante el uso de drones es excepcionalmente alta, gracias 
+                      a la capacidad de estos dispositivos para capturar imágenes de alta resolución, así como datos
+                      térmicos y de lidar. Esta información detallada y precisa proporciona una visión completa del 
+                      estado de la estructura, permitiendo tomar decisiones informadas sobre el mantenimiento y la 
+                      reparación.
+                    </p>
+                  </div>
+                  
                 </div>
-                <span className='text-sm font-medium text-yellow-500'>
-                  Tecnología 4
-                </span>
-                <span className='text-xl font-semibold'>
-                  Realidad virtual y aumentada para construcción
-                </span>
-                <span className='mt-2 text-zinc-700'>
-                  Aplicaciones:
-                  <ul className='space-y-2'>
-                    <li>• PlanAR: Superposición inmersiva de Planos en la Realidad + herramientas.</li>
-                    <li>• Eficiencia y precision en procesos de inspeccion y direccion de obra.</li>
-                    <li>• Instalaciones tecnicas asistidas (Mecánica,Electricidad y Fontanería).</li>
-                    <li>• Experiencia personalizada para el cliente.</li>
-                    <li>• Mayor engagement en un alcance global.</li>
-                    <li>• INMO HUB.</li>
-                  </ul>
-                  <Link
-                    href='/pricing'
-                    className='text-yellow-500 underline underline-offset-2'>
-                    Leer más
-                  </Link>
-                  .
-                </span>
-                
               </div>
-          </li>
-          </ol>
       </MaxWidthWrapper>
     </>
     
