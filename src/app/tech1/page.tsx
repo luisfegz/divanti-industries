98f51640adc5e94ad17a8139d04625f123818102
Luis Felipe <luisfelipegaitanzambrano@gmail.com>
2024-06-17 17:44:54 -0500

    37 XD

--- src/app/tech1/page.tsx
+++ src/app/tech1/page.tsx
@@ -19,11 +19,11 @@ const Page = () => {
             <div className='mx-auto mb-2 max-w-5xl mt-12'>
                 <div className='mb-0 px-6 lg:px-8'>
                   <div className='mx-auto max-w-2xl sm:text-center'>
-                    <p className='text-lg text-justify text-gray-600'>
+                    <p className='text-lg text-justify text-gray-600 mb-6'>
                       • Inspecciones estructurales en alturas o en grandes distancias.
                     </p>
                     <br />
-                    <div className='rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl lg:p-4' style={{ maxWidth: '800px', margin: 'auto', padding: '1rem' }}>
+                    <div className='rounded-xl mb-6 bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl lg:p-4' style={{ maxWidth: '800px', margin: 'auto', padding: '1rem' }}>
                       <Image
                         src='/tech1.jpg'
                         alt='product preview'
@@ -36,18 +36,29 @@ const Page = () => {
                     <br />
                     <p className='text-lg mt-4 text-justify text-gray-600 mb-12'>
                       Inspecciones estructurales en alturas y grandes distancias mediante drones es otra solucion
-                      que tendras a disposicion con nuestro departamento , el uso de drones representa una innovadora 
+                      que tendras a disposicion con nuestro departamento, el uso de drones representa una innovadora 
                       solución que aprovecha las tecnologías actuales para mejorar diversos aspectos clave de la 
                       gestión de infraestructuras.
                       <br /> <br />
                       En cuanto a los materiales, los drones se equipan con una variedad de sensores avanzados 
                       que permiten detectar anomalías en la estructura, como grietas, corrosión o deformaciones, 
-                      proporcionando una evaluación precisa del estado de los materiales. Esto permite una i
-                      dentificación temprana de problemas potenciales, lo que a su vez facilita la planificación 
+                      proporcionando una evaluación precisa del estado de los materiales. Esto permite una 
+                      identificación temprana de problemas potenciales, lo que a su vez facilita la planificación 
                       de mantenimiento preventivo y la prolongación de la vida útil de la infraestructura.
                       <br /> <br />
                       La costo-efectividad de esta solución radica en la reducción de los costos asociados con la 
                       mano de obra y el tiempo requerido para llevar a cabo inspecciones tradicionales. 
+                      <br /> <br />
+                      <div className='rounded-xl mb-6 bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl lg:p-4' style={{ maxWidth: '800px', margin: 'auto', padding: '1rem' }}>
+                        <Image
+                            src='/tech1-2.jpg'
+                            alt='product preview'
+                            width={1024}
+                            height={1024}
+                            quality={100}
+                            className='rounded-md bg-white shadow-2xl ring-1 ring-gray-900/10'
+                          />
+                      </div>
                       <br /> <br />
                       Al utilizar drones, se pueden inspeccionar grandes áreas en menos tiempo y con menos recursos
                       humanos, lo que resulta en una considerable reducción de los costos operativos a largo plazo.
@@ -62,6 +73,17 @@ const Page = () => {
                       térmicos y de lidar. Esta información detallada y precisa proporciona una visión completa del 
                       estado de la estructura, permitiendo tomar decisiones informadas sobre el mantenimiento y la 
                       reparación.
+                      <br /> <br />
+                      <div className='rounded-xl mb-6 bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl lg:p-4' style={{ maxWidth: '800px', margin: 'auto', padding: '1rem' }}>
+                        <Image
+                            src='/tech1-3.jpg'
+                            alt='product preview'
+                            width={1024}
+                            height={1024}
+                            quality={100}
+                            className='rounded-md bg-white shadow-2xl ring-1 ring-gray-900/10'
+                          />
+                      </div>
                     </p>
                   </div>
                   
